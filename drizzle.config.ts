95b650e509f08bea4d3f07495522759295c6767d
Niccolò Olivieri Achille <n.olivieriachille@gmail.com>
2025-04-22 18:32:01 +0200

    fix: :wrench: Using the unpooled database url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,16 +2,16 @@ import type { Config } from 'drizzle-kit';
 
 import 'dotenv/config';
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is required');
+if (!process.env.DATABASE_URL_UNPOOLED) {
+  throw new Error('DATABASE_URL_UNPOOLED environment variable is required');
 }
 
 export default {
-  schema: './lib/db/schema.ts',
+  schema: './lib/db/schema',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL_UNPOOLED,
   },
   verbose: true,
   strict: true,
